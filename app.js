7e1c0e827e576f19b742c3e68299bd8e8a8a6c85
Amadeo Abud <amaabud123@gmail.com>
2023-08-07 10:16:18 -0300

    Borrando comentarios

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const path = require('path')
 const port = process.env.PORT || 3000
-// app.use('/static', express.static(__dirname + '/public'));
 
 app.use(express.static(path.resolve(__dirname, "./public")));
 
